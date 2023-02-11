13d5379ef3932a36fe346ff7c9fb9fd255f117de
amirzangi31 <zangiabadi1378888@gmail.com>
2023-02-11 16:02:53 +0330

    add toatify package

--- components/template/AddCustomerPage.js
+++ components/template/AddCustomerPage.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Form from "../modules/Form";
 import { useRouter } from "next/router";
-import axios from "axios";
-
+import { ToastContainer } from "react-toastify";
+import Toastify from "@/services/toastify";
+import 'react-toastify/dist/ReactToastify.css';
 function AddCustomerPage() {
   const [form, setForm] = useState({
     name: "",
@@ -27,9 +28,14 @@ function AddCustomerPage() {
 
     const data = await res.json();
 
-    // const data = await axios.post("/api/customer", form);
-
-    if (data.status === "success") router.push("/");
+    if (data.status === "success") {
+      Toastify("success", "Customer registered successfully");
+      setTimeout(() => {
+        router.push("/");
+      }, 2500);
+    } else if (data.status === "failed") {
+      Toastify("error", "Invalidate data");
+    }
   };
   const cancelHandler = () => {
     setForm({
@@ -58,7 +64,9 @@ function AddCustomerPage() {
         <button className="second" onClick={saveHandler}>
           Save
         </button>
+
       </div>
+        <ToastContainer />
     </div>
   );
 }
---
+++ services/toastify.js
@@ -0,0 +1,30 @@
+import {  toast } from 'react-toastify';
+
+const Toastify = (type, text) => {
+  if (type === "success") {
+    toast.success(text, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  } else if (type === "error") {
+    console.log("amir");
+    toast.error(text, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+};
+
+export default  Toastify ;
